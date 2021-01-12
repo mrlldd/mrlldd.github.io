11da2a47d1a61b4904328c9af4461c8217173d2f
mrlldd <roflanjaraxxus@gmail.com>
2021-01-12 20:12:36 +0200

    Maybe default router will allow to open a cv without opening dashboard.

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import React from 'react';
 import './App.css';
 import BackgroundCanvas from './components/BackgroundCanvas/BackgroundCanvas';
 import Dashboard from './components/Dashboard/Dashboard';
-import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Redirect,
+  Route,
+  Router,
+  Switch,
+  useHistory,
+} from 'react-router-dom';
 import CV from './components/CV/CV';
 
 function App() {
+  const history = useHistory();
   return (
     <div className="App">
       <BackgroundCanvas>
-        <BrowserRouter>
+        <Router history={history}>
           <Switch>
             <Route exact path="/cv" component={CV} />
             <Route exact path="/" component={Dashboard} />
             <Redirect to="/" />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </BackgroundCanvas>
     </div>
   );
