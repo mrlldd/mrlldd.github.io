ddf27e78cf0cdd714af188119537598ca44d505d
mrlldd <roflanjaraxxus@gmail.com>
2021-01-12 20:03:42 +0200

    Back-button now redirects user to dashboard component, not previous page.

--- src/components/CV/CV.tsx
+++ src/components/CV/CV.tsx
@@ -93,7 +93,7 @@ const CV: React.FC = () => {
   const history = useHistory();
   return (
     <>
-      <div onClick={history.goBack} className="back-button">
+      <div onClick={() => history.push('/')} className="back-button">
         <span>{'<|'}</span>
       </div>
       <div className="CV" data-testid="CV">
